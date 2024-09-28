20d2b5a4e1fad3569ac7874983b39e8f91ed289c
Dominique Loiseau <5888729+domi7777@users.noreply.github.com>
2024-09-28 14:53:51 +0200

    refactor: switch to Phaser for better interactivity

--- src/Kit.tsx
+++ src/Kit.tsx
@@ -1,47 +1,21 @@
-import {playHiHat} from './samples/hihat.ts';
-import {playSnare} from './samples/snare.ts';
-import {playKick} from './samples/kick.ts';
-import {playCrashCymbal} from './samples/crash.ts';
+import Phaser from 'phaser';
+import {KitScene} from './scenes/KitScene.ts';
 
-const buttonWidth = '49%';
-const buttonHeight = '99%';
-
-const buttonStyle = {
-  width: buttonWidth,
-  height: buttonHeight,
-  color: 'white',
-  border: 'none',
-  borderRadius: '0',
-}
+let isGameInitialized = false;
 
 export const Kit = () => {
-  return <div style={{
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  }}>
-    <div style={{
-      display: 'flex',
-      width: '100%',
-      height: '50vh',
-      flexDirection: 'row',
-    }}>
-      <button style={{...buttonStyle, backgroundColor: '#FDA341'}} onClick={() => playHiHat()}>HiHat</button>
-      <button style={{...buttonStyle, backgroundColor: '#FE5156'}} onClick={() => playSnare()}>Snare</button>
-    </div>
-
-    <div style={{
-      display: 'flex',
-      width: '100%',
-      height: '50vh',
-      flexDirection: 'row',
-    }}>
-      <button style={{...buttonStyle, backgroundColor: '#0BDAFE'}} onClick={() => playKick()}>Kick</button>
-      <button style={{...buttonStyle, backgroundColor: '#C56BFE'}} onClick={() => playCrashCymbal()}>Crash</button>
-    </div>
-
-  </div>
+  if (!isGameInitialized) {
+    isGameInitialized = true;
+    new Phaser.Game({
+      type: Phaser.AUTO,
+      mode: Phaser.Scale.RESIZE,
+      parent: 'phaser-container',
+      width: window.innerWidth,
+      height: window.innerHeight,
+      scene: [
+        KitScene
+      ],
+    });
+    return <></>;
+  }
 }
---
+++ src/scenes/KitScene.ts
@@ -0,0 +1,36 @@
+
+import Phaser from 'phaser';
+import {playHiHat} from '../samples/hihat.ts';
+import {playSnare} from '../samples/snare.ts';
+import {playKick} from '../samples/kick.ts';
+import {playCrashCymbal} from '../samples/crash.ts';
+
+export class KitScene extends Phaser.Scene {
+
+  get gameWidth() {
+    return this.game.canvas.width * window.devicePixelRatio;
+  }
+
+  get gameHeight() {
+    return this.game.canvas.height * window.devicePixelRatio;
+  }
+
+  protected hexToColor(hex: string) {
+    return Phaser.Display.Color.HexStringToColor(hex).color;
+  }
+
+  create () {
+    this.add.rectangle(0, 0, this.gameWidth/2, this.gameHeight/2, this.hexToColor('#FDA341'))
+      .setInteractive()
+      .on('pointerdown', () =>  playHiHat());
+    this.add.rectangle(this.gameWidth/2, 0, this.gameWidth/2, this.gameHeight/2, this.hexToColor('#FE5156'))
+      .setInteractive()
+      .on('pointerdown', () =>  playSnare());
+    this.add.rectangle(0, this.gameHeight/2, this.gameWidth/2, this.gameHeight/2, this.hexToColor('#4A90E2'))
+      .setInteractive()
+      .on('pointerdown', () =>  playKick());
+    this.add.rectangle(this.gameWidth/2, this.gameHeight/2, this.gameWidth/2, this.gameHeight/2, this.hexToColor('#C56BFE'))
+      .setInteractive()
+      .on('pointerdown', () =>  playCrashCymbal());
+  }
+}
