83aa58b9d1ba39ee0fbab26b6c906d2ba5d9fc68
Dominique Loiseau <5888729+domi7777@users.noreply.github.com>
2024-10-03 22:26:15 +0200

    feat(controls): add stop / record / play buttons (not doing anything atm)

--- src/scenes/KitScene.ts
+++ src/scenes/KitScene.ts
@@ -5,8 +5,49 @@ import {playSnare} from '../samples/snare.ts';
 import {playKick} from '../samples/kick.ts';
 import {playCrashCymbal} from '../samples/crash.ts';
 
+function startRecording() {
+  console.log('Recording started');
+}
+
+function stopRecording() {
+  console.log('Recording stopped');
+}
+
+function startPlaying() {
+  console.log('Loop played');
+}
+
+function stopPlaying() {
+  console.log('Loop stopped');
+}
+
+type ControlButton = {
+  button: Phaser.GameObjects.Rectangle,
+  text: Phaser.GameObjects.Text,
+};
+
+type HexaColor = `#${string}`;
+
+const controlFonts: Record<ControlState, HexaColor> = {
+  idle: '#FFF',
+  readyToRecord: '#0FF',
+  recording: '#FD0041',
+  playing: '#0F0',
+}
+
+const colors = {
+  fonts: controlFonts
+}
+
+type ControlState = 'idle' | 'readyToRecord' | 'recording' | 'playing';
+
 export class KitScene extends Phaser.Scene {
-  private recordButton!: Phaser.GameObjects.Arc;
+  private controls: {
+    state: ControlState,
+    stop: ControlButton,
+    record: ControlButton,
+    play: ControlButton,
+  }
 
   protected hexToColor(hex: string) {
     return Phaser.Display.Color.HexStringToColor(hex).color;
@@ -14,7 +55,7 @@ export class KitScene extends Phaser.Scene {
 
   create () {
     this.createPads();
-    this.createRecordButton();
+    this.createControlButtons();
   }
 
   private createPads() {
@@ -24,17 +65,19 @@ export class KitScene extends Phaser.Scene {
       this.add.rectangle().setFillStyle(this.hexToColor('#4A90E2')).on('pointerdown', () => playSnare()),
       this.add.rectangle().setFillStyle(this.hexToColor('#A0D8C5')).on('pointerdown', () => playCrashCymbal())
     ];
+
     pads.forEach(rectangle => rectangle
       .setInteractive()
       .setOrigin(0, 0)
       .on('pointerdown', () => {
         rectangle.setAlpha(0.7);
-        this.tweens.add({
-          targets: rectangle,
-          alpha: 1,
-          duration: 250
-        });
-      })
+        if (this.controls.state === 'readyToRecord') {
+          this.controls.state = 'recording';
+          this.updateControlsText();
+          startRecording();
+        }
+      }).on('pointerup', () => rectangle.setAlpha(1))
+      .on('pointerout', () => rectangle.setAlpha(1))
     );
 
     const resizePads = () => {
@@ -50,54 +93,119 @@ export class KitScene extends Phaser.Scene {
     resizePads();
   }
 
-  private createRecordButton() {
-    const recordRectangle = this.add.rectangle()
-      .setFillStyle(this.hexToColor('#000000'))
-      .setOrigin(0, 0)
-    this.recordButton = this.add.circle()
-      .setFillStyle(this.hexToColor('#FD0041'))
-      .setOrigin(0.5, 0)
-    let isRecordingReady = false;
-    let blinkingRecordButtonTween: Phaser.Tweens.Tween;
-    [recordRectangle, this.recordButton].forEach(button => {
-      button.setInteractive()
-        .on('pointerdown', () => {
-          isRecordingReady = !isRecordingReady;
-          this.recordButton.setAlpha(0.7);
-          this.tweens.add({
-            targets: this.recordButton,
-            alpha: 1,
-            duration: 250
-          });
+  private createButton() {
+    const button =  this.add.rectangle()
+      .setFillStyle(this.hexToColor('#000'))
+      .setStrokeStyle(2, this.hexToColor('#FFF'), 0.8)
+      .setOrigin(0, 0);
+    button.setInteractive()
+      .on('pointerdown', () => button.setFillStyle(this.hexToColor('#666')))
+      .on('pointerup', () => button.setFillStyle(this.hexToColor('#000')));
+    return button;
+  }
 
-          if (isRecordingReady) {
-            blinkingRecordButtonTween = this.tweens.add({
-              targets: this.recordButton,
-              alpha: 0.5,
-              duration: 750,
-              repeat: -1,
-              yoyo: true,
-              ease: 'Sine.easeInOut'
-            });
-          } else {
-            blinkingRecordButtonTween.stop();
-            this.recordButton.setAlpha(1);
-          }
-        });
-    });
+  private createText(text: string) {
+    return this.add.text(0, 0, text)
+      .setOrigin(0.5, 0.5)
+      .setFontSize(20)
+      .setFontFamily('Arial')
+      .setAlign('center')
+      .setColor('#FFF')
+      .setData('initial', text);
+  }
 
-    const resizeRecordButton = () => {
+  private createControlButtons() {
+    this.controls = {
+      state: 'idle',
+      stop: {
+        button: this.createButton(),
+        text: this.createText('Stop')
+      },
+      record: {
+        button: this.createButton(),
+        text: this.createText('Record')
+      },
+      play: {
+        button: this.createButton(),
+        text: this.createText('Play')
+      },
+    }
+
+    const {record, stop, play} = this.controls;
+    record.button.setInteractive()
+      .on('pointerdown', () => {
+        if (this.controls.state === 'idle' || this.controls.state === 'playing') {
+          this.controls.state = 'readyToRecord';
+          record.text.setText('Hit a pad to start');
+        } else if (this.controls.state === 'readyToRecord') {
+          this.controls.state = 'idle';
+          record.text.setText(record.text.getData('initial'));
+          //this.recordButton.setFillStyle(this.hexToColor('#AAA'));
+        } else if (this.controls.state === 'recording') {
+          this.controls.state = 'idle';
+          stopRecording();
+        }
+        this.updateControlsText();
+      });
+    stop.button.setInteractive()
+      .on('pointerdown', () => {
+        this.controls.state = 'idle';
+        if (this.controls.state === 'recording') {
+          stopRecording();
+        }
+        if (this.controls.state === 'playing') {
+          stopPlaying();
+        }
+        this.updateControlsText();
+      });
+    play.button.setInteractive()
+      .on('pointerdown', () => {
+        this.controls.state = 'playing';
+        // TODO show text record loop first when no loop
+        startPlaying();
+        this.updateControlsText();
+      });
+
+    const resizeControls = () => {
       const width = window.innerWidth;
       const height = window.innerHeight;
-      let recordButtonSize = Math.max(height, width) / 20;
-      recordRectangle.setSize(width, recordButtonSize).setPosition(0, -1);
-      this.recordButton
-        .setRadius((recordButtonSize / 2) - 5)
-        .setPosition(width / 2, 5)
-        .setStrokeStyle(recordButtonSize / 20, this.hexToColor('#FFF'), 0.8)
+      const buttonHeight = Math.max(height, width) / 15;
+      const buttonWidth = width / 3;
 
+      [stop, record, play].forEach(({button, text}, index) => {
+        button.setSize(buttonWidth, buttonHeight).setPosition(buttonWidth * index, -1);
+        text.setFontSize(buttonHeight / 3)
+          .setWordWrapWidth(button.width, true)
+          .setSize(button.width, button.height)
+          .setPosition(button.getCenter().x, button.getCenter().y);
+      });
     };
-    window.addEventListener('resize', resizeRecordButton);
-    resizeRecordButton();
+    window.addEventListener('resize', resizeControls);
+    resizeControls();
+  }
+
+  private updateControlsText() {
+    const {
+      record: {text: recordText},
+      stop: {text: stopText},
+      play: {text: playText}
+    } = this.controls;
+    [recordText, stopText, playText]
+      .forEach(text => text.setText(text.getData('initial')).setColor(colors.fonts.idle));
+    const color = colors.fonts[this.controls.state];
+    switch (this.controls.state) {
+    case 'idle':
+      break;
+    case 'readyToRecord':
+      recordText.setText('Hit a pad to start').setColor(color);
+      break;
+    case 'recording':
+      recordText.setText('Recording...').setColor(color);
+      break;
+    case 'playing':
+      playText.setText('Playing...').setColor(color);
+      break;
+    }
+
   }
 }
