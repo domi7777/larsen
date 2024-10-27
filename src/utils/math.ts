9a2bacaaa7017946af7ce63533058b45cdef4c7c
Dominique Loiseau <5888729+domi7777@users.noreply.github.com>
2024-10-27 21:46:50 +0100

    refactor(naming): rename var

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,7 +1,7 @@
-export const rotateArray = <T>(pads: T[], cols: number, rows: number): T[] => {
+export const rotateArray = <T>(array: T[], cols: number, rows: number): T[] => {
   const matrix: T[][] = [];
   for (let i = 0; i < rows; i++) {
-    matrix.push(pads.slice(i * cols, i * cols + cols));
+    matrix.push(array.slice(i * cols, i * cols + cols));
   }
   const rotated = matrix[0].map((_val, index) => matrix.map(row => row[index]).reverse());
   return rotated.flat();
