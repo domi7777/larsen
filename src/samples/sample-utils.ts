97a8dc0bb8b30e02f4648bf0548183ed17fc9185
Dominique Loiseau <5888729+domi7777@users.noreply.github.com>
2024-11-29 10:36:07 +0100

    fix(mobile): try to resume audioContext after minimizing app

--- src/samples/sample-utils.ts
+++ src/samples/sample-utils.ts
@@ -1,14 +1,27 @@
+import {logger} from '../utils/logger.ts';
+
 let audioContext: AudioContext | null = null;
+const AudioContext = (window as any).AudioContext || (window as any).webkitAudioContext;
 
 export const createAudioContext = () => {
   // reuse the existing AudioContext to avoid browser restriction when creating audio context in loop with no user interaction
   if (!audioContext) {
-    const AudioContext = (window as any).AudioContext || (window as any).webkitAudioContext;
     audioContext = new AudioContext();
+  }
+  if (audioContext?.state === 'suspended') {
+    logger.log('Resuming AudioContext');
+    audioContext?.resume();
   }
   return audioContext!;
 }
 
 export const resetAudioContext = () => {
-  audioContext = null;
+  // audioContext = null;
 }
+
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'visible' && audioContext?.state === 'suspended') {
+    logger.log('Resuming AudioContext after visibility change');
+    audioContext?.resume().catch((err) => logger.error('Error resuming AudioContext:', err));
+  }
+});
